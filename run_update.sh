c4d48127fa93d185a04da6431bee041f18ffe818
amtseng <amtseng@stanford.edu>
2017-11-08 17:27:22 -0800

    Wrapper script to run update and email results

---
+++ run_update.sh
@@ -0,0 +1,20 @@
+if [ -z $1 ]
+then
+    echo "Usage:"
+    echo "sh run_update.sh <email_address>"
+    exit 0
+fi
+
+# Define some file paths
+json="/cluster/u/amtseng/dbgap_scrape/data/studies_cron.json"
+diff="/cluster/u/amtseng/dbgap_scrape/data/diff_cron.txt"
+email="/cluster/u/amtseng/dbgap_scrape/data/email_cron.txt"
+
+# Run scraper
+python /cluster/u/amtseng/dbgap_scrape/main.py -i $json -o $json -u $diff -v
+
+# Email contents of diff
+echo "Subject: dbGaP scrape: new studies and updates" > $email
+cat $diff >> $email
+sendmail $1 < $email
+rm $email
